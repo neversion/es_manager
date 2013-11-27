d454d67026ff00a695a0aded0533ae22387eb9b2
neversion <neversionchen@gmail.com>
2013-11-27 21:45:22 +0800

    遍历获取文件，xml解析

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,24 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 def import_test_data path
-
+  count=0
+  file_name_list=[]
+  Dir.foreach(path) do |file|
+  file_name_list << file
+  end
+ 
+  File.open "#{path}/#{file_name_list[2]}" do |file|
+  xml_str=''   
+ file.each_line do |line|
+ xml_str = xml_str+line
 end
+
+xml_doc  = Nokogiri::XML(xml_str)
+puts xml_str
+binding.pry
+xml_doc.xpath('//dc:creator').text
+end
+end
+
+import_test_data "/hd/metadata/data/guji"
+
