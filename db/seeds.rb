b1d8308c2dac8506f0d60c238623c800e2cc8edd
neversion <neversionchen@gmail.com>
2013-11-28 09:47:23 +0800

    重复字段问题 增加origin_date字段

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,6 @@ def import_test_data path
         xml_str = xml_str+line
       end
       xml_doc = Nokogiri::XML(xml_str)
-      binding.pry
       parsed_date = ''
       begin
         parsed_date = Date.parse(xml_doc.xpath('//dc:date').text) unless xml_doc.xpath('//dc:date').text==''
@@ -22,52 +21,62 @@ def import_test_data path
       end
       if parsed_date.length!=0
         body_json = {
-            "title" => xml_doc.xpath('//dc:title').text,
-            "creator" => xml_doc.xpath('//dc:creator').text,
-            "subject" => xml_doc.xpath('//dc:subject').text,
-            "descripton" => xml_doc.xpath('//dc:descripton').text,
-            "publisher" => xml_doc.xpath('//dc:publisher').text,
-            "contributor" => xml_doc.xpath('//dc:contributor').text,
-            "date" => xml_doc.xpath('//dc:date').text,
-            "type" => xml_doc.xpath('//dc:type').text,
-            "format" => xml_doc.xpath('//dc:format').text,
-            "identifier" => xml_doc.xpath('//dc:identifier').text,
-            "source" => xml_doc.xpath('//dc:source').text,
-            "language" => xml_doc.xpath('//dc:language').text,
-            "relation" => xml_doc.xpath('//dc:relation').text,
-            "coverage" => xml_doc.xpath('//dc:coverage').text,
-            "rights" => xml_doc.xpath('//dc:rights').text,
+            "title" => get_field_value(xml_doc, '//dc:title'),
+            "creator" => get_field_value(xml_doc, '//dc:creator'),
+            "subject" => get_field_value(xml_doc, '//dc:subject'),
+            "descripton" => get_field_value(xml_doc, '//dc:descripton'),
+            "publisher" => get_field_value(xml_doc, '//dc:publisher'),
+            "contributor" => get_field_value(xml_doc, '//dc:contributor'),
+            "date" => get_field_value(xml_doc, '//dc:date'),
+            "type" => get_field_value(xml_doc, '//dc:type'),
+            "format" => get_field_value(xml_doc, '//dc:format'),
+            "identifier" => get_field_value(xml_doc, '//dc:identifier'),
+            "source" => get_field_value(xml_doc, '//dc:source'),
+            "language" => get_field_value(xml_doc, '//dc:language'),
+            "relation" => get_field_value(xml_doc, '//dc:relation'),
+            "coverage" => get_field_value(xml_doc, '//dc:coverage'),
+            "rights" => get_field_value(xml_doc, '//dc:rights'),
             "harvest_time" => Date.new
         }
       else
         body_json = {
-            "title" => xml_doc.xpath('//dc:title').text,
-            "creator" => xml_doc.xpath('//dc:creator').text,
-            "subject" => xml_doc.xpath('//dc:subject').text,
-            "descripton" => xml_doc.xpath('//dc:descripton').text,
-            "publisher" => xml_doc.xpath('//dc:publisher').text,
-            "contributor" => xml_doc.xpath('//dc:contributor').text,
-            "type" => xml_doc.xpath('//dc:type').text,
-            "format" => xml_doc.xpath('//dc:format').text,
-            "identifier" => xml_doc.xpath('//dc:identifier').text,
-            "source" => xml_doc.xpath('//dc:source').text,
-            "language" => xml_doc.xpath('//dc:language').text,
-            "relation" => xml_doc.xpath('//dc:relation').text,
-            "coverage" => xml_doc.xpath('//dc:coverage').text,
-            "rights" => xml_doc.xpath('//dc:rights').text,
+            "title" => get_field_value(xml_doc, '//dc:title'),
+            "creator" => get_field_value(xml_doc, '//dc:creator'),
+            "subject" => get_field_value(xml_doc, '//dc:subject'),
+            "descripton" => get_field_value(xml_doc, '//dc:descripton'),
+            "publisher" => get_field_value(xml_doc, '//dc:publisher'),
+            "contributor" => get_field_value(xml_doc, '//dc:contributor'),
+            "origin_date" => get_field_value(xml_doc, '//dc:date'),
+            "type" => get_field_value(xml_doc, '//dc:type'),
+            "format" => get_field_value(xml_doc, '//dc:format'),
+            "identifier" => get_field_value(xml_doc, '//dc:identifier'),
+            "source" => get_field_value(xml_doc, '//dc:source'),
+            "language" => get_field_value(xml_doc, '//dc:language'),
+            "relation" => get_field_value(xml_doc, '//dc:relation'),
+            "coverage" => get_field_value(xml_doc, '//dc:coverage'),
+            "rights" => get_field_value(xml_doc, '//dc:rights'),
             "harvest_time" => Date.new
         }
       end
       @client.index index: 'oai', type: 'item', body: body_json
-
-
     end
   end
 
 end
 
+def get_field_value xml_doc, xpath
+  result=''
+  xml_doc.xpath(xpath).each_with_index do |item,index|
+    if index==0
+      result=item.text
+    else
+      result = "#{result}|||#{item.text}"
+    end
+  end
+  return result
+end
 
-def mapping
+def mapping_with_new_index
   host = "http://210.34.4.113:9200"
   @client = Elasticsearch::Client.new host: host, log: true
   @client.indices.create index: 'oai',
@@ -104,6 +113,32 @@ def mapping
 
 end
 
-import_test_data "/hd/metadata/data/guji"
-#mapping
+def update_mapping index_name
+  host = "http://210.34.4.113:9200"
+  client = Elasticsearch::Client.new host: host, log: true
+  client.indices.put_mapping index: index_name, type: 'item', body: {
+      item: {
+          properties: {
+              title: {type: 'string', analyzer: 'snowball', store: 'yes', boost: 3.0},
+              creator: {type: 'string', analyzer: 'snowball', store: 'yes'},
+              subject: {type: 'string', analyzer: 'snowball', store: 'yes'},
+              description: {type: 'string', analyzer: 'snowball', store: 'yes'},
+              publisher: {type: 'string', store: 'yes'},
+              contributor: {type: 'string', analyzer: 'snowball', store: 'yes'},
+              date: {type: 'date', store: 'yes'},
+              origin_date: {type: 'string',index: 'no', store: 'yes'},
+              type: {type: 'string', analyzer: 'snowball', store: 'yes'},
+              format: {type: 'string', index: 'not_analyzed', store: 'yes'},
+              identifier: {type: 'string', index: 'not_analyzed', store: 'yes'},
+              source: {type: 'string', index: 'not_analyzed', store: 'yes'},
+              language: {type: 'string', index: 'not_analyzed', store: 'yes'},
+              relation: {type: 'string', store: 'yes'},
+              coverage: {type: 'string', index: 'no', store: 'yes'},
+              rights: {type: 'string', index: 'not_analyzed', store: 'yes'},
+              harvest_time: {type: 'date', store: 'yes'},
+          }
+      }
+  }
+end
 
+import_test_data "/hd/metadata/data/guji"
