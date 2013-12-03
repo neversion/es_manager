e8730bf47648081c00da013813be9ac7d1a76943
neversion <neversionchen@gmail.com>
2013-12-03 10:51:07 +0800

    fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,5 @@ EsManager::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.action_controller.allow_forgery_protection = false
 end
