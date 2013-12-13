f7b48bc034bd5032a0db6c48a1326686b1062de6
neversion <neversionchen@gmail.com>
2013-12-13 10:22:26 +0800

    bulk import

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require "#{Rails.root}/db/oai_import.rb"
 require 'benchmark'
 
-#create_index_mapping "oai_ik_stem_all"
-puts Benchmark.measure{ import_oai_test_data "oai_ik_stem_all","/hd/metadata/data/apabi" }
+#create_index_mapping "oai_ik_stem_all_bulk"
+#puts Benchmark.measure{ import_oai_test_data "oai_ik_stem_all","/hd/metadata/data/apabi" }
+puts Benchmark.measure{ import_bulk "oai_ik_stem_all_bulk","/hd/metadata/data/apabi" }
