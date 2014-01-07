974206e60ffb1459cc982ad77e3ecddb01404d5c
neversion <neversionchen@gmail.com>
2014-01-07 09:19:43 +0800

    fix css

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,4 @@ require 'benchmark'
 #create_index_mapping "oai_all"
 #puts Benchmark.measure{ import_oai_test_data "oai_ik_stem_all","/hd/metadata/data/apabi" }
 #puts Benchmark.measure{ import_bulk "oai_ik_stem_all_bulk","/hd/metadata/data/asia" }
-puts Benchmark.measure{ import_bulk "oai_all","/oai_metadata/data/opac" }
+puts Benchmark.measure{ import_bulk "oai_all","/oai_metadata/data/log" }
